0b8f6fab3432ed88e9001b83f22e106602b524e5
jeganpillai <jegan.velappan@gmail.com>
2024-03-16 16:45:00 -0700

    Create p0054_filtering_dictionary_keys_by_value.py
    initial version

---
+++ p0054_filtering_dictionary_keys_by_value.py
@@ -0,0 +1,21 @@
+# Question: filtering dictionary keys by value
+# Video: https://www.youtube.com/watch?v=13cHR4ucRYg
+
+# *** Approach 1: Simple looping option *** 
+d = {'Apple':'Fruit', 'Strawberry':'Fruit', 'Peas':'Veggie', 'Tomato':'Veggie', 'Lettuce':'Veggie'}
+value = 'Fruit'
+def Grow_With_Data(d, value):
+    result = []
+    for key, val in d.items():
+        if val == value:
+            result.append(key)
+    return result
+Grow_With_Data(d, value)
+
+# *** Approach 2: Using List Comprehension process *** 
+d = {'Apple':'Fruit', 'Strawberry':'Fruit', 'Peas':'Veggie', 'Tomato':'Veggie', 'Lettuce':'Veggie'}
+value = 'Fruit'
+def Grow_With_Data(d, value):
+    result = [key for key, val in d.items() if val == value]
+    return result
+Grow_With_Data(d, value)
