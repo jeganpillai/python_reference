bdea9739ffcd0d6610335960cc21225c7e211385
jeganpillai <jegan.velappan@gmail.com>
2024-03-26 16:12:23 -0700

    Update p0052_check_if_the_sentence_is_pangram.py
    updated with sample data

--- p0052_check_if_the_sentence_is_pangram.py
+++ p0052_check_if_the_sentence_is_pangram.py
@@ -1,13 +1,22 @@
 # Question: Check if the sentence is pangram.
 # Video: https://www.youtube.com/watch?v=rSRkiDKfbNY
 
+sentence = "thequickbrownfoxjumpsoverthelazydog"
+sentence = "growwithdata"
+
 *** Approach 1: convert to list and set then compare the length *** 
 def Grow_With_Data(sentence):
     return len(set(list(sentence))) == 26
+assert Grow_With_Data("thequickbrownfoxjumpsoverthelazydog") == True
+assert Grow_With_Data("growwithdata") == False
+print("Passed!")
 
 *** Approach 2: convert directly to set and then compare the length *** 
 def Grow_With_Data(sentence):
     return len(set(sentence)) == 26
+assert Grow_With_Data("thequickbrownfoxjumpsoverthelazydog") == True
+assert Grow_With_Data("growwithdata") == False
+print("Passed!")
 
 *** Approach 3: using dictionary *** 
 def Grow_With_Data(sentence):
@@ -15,6 +24,9 @@ def Grow_With_Data(sentence):
     for i in sentence:
         d[i] = d.get(i,0) + 1
     return len(d) == 26
+assert Grow_With_Data("thequickbrownfoxjumpsoverthelazydog") == True
+assert Grow_With_Data("growwithdata") == False
+print("Passed!")
 
 *** Approach 4: using list and loop *** 
 def Grow_With_Data(sentence):
@@ -23,9 +35,15 @@ def Grow_With_Data(sentence):
         if not i in result:
             result.append(i)
     return len(result) == 26
+assert Grow_With_Data("thequickbrownfoxjumpsoverthelazydog") == True
+assert Grow_With_Data("growwithdata") == False
+print("Passed!")
 
 *** Approach 5: sorted list option *** 
 def Grow_With_Data(sentence):
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     s = sorted(list(set(sentence)))
     return s == alpha
+assert Grow_With_Data("thequickbrownfoxjumpsoverthelazydog") == True
+assert Grow_With_Data("growwithdata") == False
+print("Passed!")
