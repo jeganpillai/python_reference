412323e02e30847e821a8e7a03abb5122ed27d5f
jeganpillai <jegan.velappan@gmail.com>
2024-04-01 16:22:39 -0700

    Create p0061_harshad_or_niven_number.py
    initial version

---
+++ p0061_harshad_or_niven_number.py
@@ -0,0 +1,37 @@
+# Question: Find if the given number is a Harshad number or not.
+
+# English Video: 
+# Tamil Video: 
+
+# Approach 1: General Looping logic 
+def Grow_With_Data(num):
+    chk = 0
+    for i in str(num):
+        chk += int(i)
+    if num % chk == 0:
+        return chk
+    else:
+        return -1
+assert Grow_With_Data(18) == 9
+assert Grow_With_Data(23) == -1
+print('Passed!')
+
+# Approach 2: Using List Comprehension 
+def Grow_With_Data(num):
+    chk = sum([int(i) for i in str(num)])
+    return chk if num / chk == num // chk else -1
+assert Grow_With_Data(18) == 9
+assert Grow_With_Data(23) == -1
+print('Passed!')
+
+# Approach 3: Using While loop
+def Grow_With_Data(num):
+    chk = 0 
+    x = num
+    while num > 0:
+        chk += num % 10
+        num //= 10
+    return chk if x / chk == x // chk else -1
+assert Grow_With_Data(18) == 9
+assert Grow_With_Data(23) == -1
+print('Passed!')
