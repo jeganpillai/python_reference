2482765656cfc87fb6df0d8e0f2dfe247832e003
jeganpillai <jegan.velappan@gmail.com>
2024-04-21 19:21:13 -0700

    Create p0070_remove_duplicates_from_sorted_array.py

---
+++ p0070_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,34 @@
+# Question: Remove Duplicates from Sorted Array
+
+# English Video: 
+# Tamil Video: 
+
+# Approach 1: In-place approach with additional set() data type
+def Grow_With_Data(nums):
+    if not nums:
+        return 0
+    chk = set()
+    result = 0
+    for num in nums:
+        if num not in chk:
+            chk.add(num)
+            nums[result] = num
+            result += 1
+    return result
+assert Grow_With_Data([1,1,2]) == 2
+assert Grow_With_Data([0,0,1,1,1,2,2,3,3,4]) == 5
+print('Passed!')
+
+# Approach 2: In-place with no additional array stored
+def Grow_With_Data(nums):
+    if not nums:
+        return 0
+    result = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[result]:
+            result += 1
+            nums[result] = nums[j]
+    return result + 1
+assert Grow_With_Data([1,1,2]) == 2
+assert Grow_With_Data([0,0,1,1,1,2,2,3,3,4]) == 5
+print('Passed!')
