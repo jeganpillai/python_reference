712f91e13b4251bdc73038d928a36c585989a7ad
jeganpillai <jegan.velappan@gmail.com>
2024-04-29 23:47:59 -0700

    Update p0074_string_to_balanced_substrings.py
    cosmetic change

--- p0074_string_to_balanced_substrings.py
+++ p0074_string_to_balanced_substrings.py
@@ -8,7 +8,7 @@ def Grow_With_Data(s):
         if c == 'R': r += 1
         if c == 'L': l += 1
         if l == r: count += 1
-    return res
+    return count
 Grow_With_Data(s)
 assert Grow_With_Data('RLRRLLRLRL') == 4
 assert Grow_With_Data('RLRRRLLRLL') == 2
@@ -18,12 +18,12 @@ print('Passed!')
 # Approach 2: Track L and R using single variable 
 def Grow_With_Data(s):
     balance = 0
-    res = 0
+    count = 0
     for c in s:
         if c == 'R': balance += 1
         if c == 'L': balance -= 1
-        if balance == 0: res += 1
-    return res
+        if balance == 0: count += 1
+    return count
 Grow_With_Data(s)
 assert Grow_With_Data('RLRRLLRLRL') == 4
 assert Grow_With_Data('RLRRRLLRLL') == 2
@@ -33,12 +33,12 @@ print('Passed!')
 # Approach 3: Simplified version 
 def Grow_With_Data(s):
     balance = 0
-    res = 0
+    count = 0
     for c in s:
         # balance = balance +1 if c == 'R' else balance -1
         balance += 1 if c == 'R' else -1
-        if balance == 0: res += 1
-    return res
+        if balance == 0: count += 1
+    return count
 Grow_With_Data(s)
 assert Grow_With_Data('RLRRLLRLRL') == 4
 assert Grow_With_Data('RLRRRLLRLL') == 2
