10f73617216f9867ef890dea0a48cca8193a1844
jeganpillai <jegan.velappan@gmail.com>
2024-05-16 00:32:47 -0700

    Create p0082_latest_time_correction.py
    initial version

---
+++ p0082_latest_time_correction.py
@@ -0,0 +1,40 @@
+# Question: Decode a message
+
+# English Video: 
+# Tamil Video: 
+
+-- Approach 1: Basic looping logic
+def Grow_With_Data(key, message):
+    dict_key = {}
+    cnt = 0
+    for val in key:
+        if val not in dict_key and not val == ' ':
+            dict_key[val] = chr(97 + cnt)
+            cnt += 1
+    result = []
+    for m in message:
+        if m == ' ':
+            result.append(m)
+        else:
+            result.append(dict_key[m])
+    return ''.join(result)
+Grow_With_Data('thequickbrownfxjmpsvlazydg', 'vkbs bs t suepuv') == 'this is a secret'
+Grow_With_Data('eljuxhpwnyrdgtqkviszcfmabo','zwx hnfx lqantp mnoeius ycgk vcnjrdb') == 'the five boxing wizards jump quickly'
+print('Passed!')
+
+
+-- Approach 2: Smarter coding 
+def Grow_With_Data(key, message):
+    dict_key = {}
+    cnt = 0
+    for val in key:
+        if val not in dict_key and not val == ' ':
+            dict_key[val] = chr(97 + cnt)
+            cnt += 1
+    result = []
+    for m in message:
+        result.append(m if m == ' ' else dict_key[m])
+    return ''.join(result)
+Grow_With_Data('thequickbrownfxjmpsvlazydg', 'vkbs bs t suepuv') == 'this is a secret'
+Grow_With_Data('eljuxhpwnyrdgtqkviszcfmabo','zwx hnfx lqantp mnoeius ycgk vcnjrdb') == 'the five boxing wizards jump quickly'
+print('Passed!')
