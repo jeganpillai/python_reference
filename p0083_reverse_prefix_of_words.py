12d9cfb97f6c58377320a8238cca93b3d812fc80
jeganpillai <jegan.velappan@gmail.com>
2024-05-20 00:51:24 -0700

    Create p0083_reverse_prefix_of_words.py
    initial version

---
+++ p0083_reverse_prefix_of_words.py
@@ -0,0 +1,29 @@
+# Question: Reverse prefix of words 
+
+# English Video: 
+# Tamil Video: 
+
+# Approach 1: Using find() and String Slicing 
+def Grow_With_Data(word, ch):
+    slice = word.find(ch)
+    if slice == -1:
+        return word
+    else:
+        return word[: word.find(ch) + 1][::-1]+ word[word.find(ch) + 1:]
+assert Grow_With_Data('abcdefd', 'd') == 'dcbaefd'
+assert Grow_With_Data('xyxzxe', 'z') == 'zxyxxe'
+assert Grow_With_Data('abcd', 'z') == 'abcd'
+print('Passed!')
+
+# Approach 2: Using index() and reversed() Function
+def Grow_With_Data(word, ch):
+    if ch in word:
+        pos = word.index(ch)
+        return ''.join(list((reversed(word[: pos + 1])))) + word[pos + 1:]
+    else:
+        return word
+        
+assert Grow_With_Data('abcdefd', 'd') == 'dcbaefd'
+assert Grow_With_Data('xyxzxe', 'z') == 'zxyxxe'
+assert Grow_With_Data('abcd', 'z') == 'abcd'
+print('Passed!')
