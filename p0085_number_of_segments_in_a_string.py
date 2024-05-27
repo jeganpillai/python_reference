1ade56a7bec0b51826110377aff2adf99f50ada6
jeganpillai <jegan.velappan@gmail.com>
2024-05-26 19:00:20 -0700

    Create p0085_number_of_segments_in_a_string.py
    initial version

---
+++ p0085_number_of_segments_in_a_string.py
@@ -0,0 +1,36 @@
+# Question: Find the Number of Segments in a String
+
+# English Video: 
+# Tamil Video: 
+
+# Approach 1: Looping logic 
+def Grow_With_Data(s):
+    cnt = 0
+    chk = ''
+    for indx, val in enumerate(s):
+        if val == ' ':
+            if chk:
+                cnt += 1
+                chk = ''
+            else:
+                continue
+        else:
+            chk += val
+    return cnt + 1 if chk else cnt
+assert Grow_With_Data(', , , ,        a, eaefa') == 6
+assert Grow_With_Data('') == 0
+assert Grow_With_Data('Of all the gin joints in all the towns in all the world,   ') == 13
+assert Grow_With_Data('Hello, my name is John') == 5
+assert Grow_With_Data('Hello') == 1
+print('Passed!')
+
+# Approach 2: Using inbuilt function split()
+def Grow_With_Data(s):
+    return len(s.split())
+assert Grow_With_Data(', , , ,        a, eaefa') == 6
+assert Grow_With_Data('') == 0
+assert Grow_With_Data('Of all the gin joints in all the towns in all the world,   ') == 13
+assert Grow_With_Data('Hello, my name is John') == 5
+assert Grow_With_Data('Hello') == 1
+print('Passed!')
+
